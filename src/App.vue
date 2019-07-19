3a1ed6db05f643036db1621cc431af5c29d4e2c7
David Maccarone <noreply@git.email>
2019-07-19 17:53:23 +1000

    Added front-end files

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,65 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="text-gray-800 md:text-lg italic font-light">Which is your favourite AWS serverless service?</div>
+    <div class="mx-auto flex mt-4">
+      <button v-for="(candidate, index) in candidates" v-bind:key="candidate.id" @click="castVote(candidate.id)"
+        v-bind:class="`focus:outline-none flex-1 h-max text-white text-sm sm:text-lg font-semibold py-2 px-2 mx-1 md:h-18 h-18 rounded bg-${candidateColors[index]}-600 hover:bg-${candidateColors[index]}-800`">
+        {{candidate.name}}<p>{{candidate.votes}}</p>
+      </button>
+    </div>
+    <h1 class="text-gray-800 font-bold mt-8 mb-3">Live updates</h1>
+    <VoteChart v-bind:chartData="chartData" width="200" height="110"></VoteChart>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VoteChart from "./components/VoteChart";
+import { API, graphqlOperation } from "aws-amplify";
+import * as queries from "./graphql/queries";
+import * as mutations from "./graphql/mutations";
+import * as subscriptions from "./graphql/subscriptions";
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  components: { VoteChart },
+  data() {
+    return {
+      candidates: [],
+      candidateColors: ["red", "orange", "green", "blue"]
+    };
+  },
+  computed: {
+    chartData: function() {
+      return {
+        labels: this.candidates.map(candidate => candidate.name),
+        datasets: [{
+            label: false,
+            data: this.candidates.map(candidate => candidate.votes),
+            backgroundColor: ["#e53e3e", "#dd6b20", "#38a169", "#3182ce"]
+          }]};
+    }
+  },
+  async created() {
+    await API.graphql(graphqlOperation(queries.listCandidates)).then(
+      res => (this.candidates = res.data.listCandidates.items)
+    );
+  },
+  methods: {
+    castVote(candidateId) {
+      const vote = { id: candidateId };
+      API.graphql(graphqlOperation(mutations.castVote, { input: vote }));
+    }
+  },
+  mounted() {
+    API.graphql(graphqlOperation(subscriptions.onCastVote)).subscribe({
+      next: voteCasted => {
+        const id = voteCasted.value.data.onCastVote.id;
+        const votes = voteCasted.value.data.onCastVote.votes;
+        const candidates = this.candidates;
+        const candidate = candidates.find(candidate => candidate.id === id);
+        candidate.votes = votes;
+        this.candidates = candidates;
+      }
+    });
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/VoteChart.vue
@@ -0,0 +1,29 @@
+<script>
+import { Bar, mixins } from "vue-chartjs";
+const { reactiveProp } = mixins;
+const fontStack = '-apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"';
+const chartOptions = {
+  title: { display: false },
+  legend: { display: false },
+  tooltips: { enabled: false },
+  responsive: true,
+  layout: { padding: { top: 10 } },
+  scales: {
+    xAxes: [{gridLines: {display: false }, ticks: {fontStyle: 'bold', fontSize:13, fontColor: '#3D4852', fontFamily: fontStack}}],
+    yAxes: [{ticks: {beginAtZero: true, maxTicksLimit: 6, fontStyle: 'normal', fontColor: '#3D4852', fontFamily: fontStack}}]
+  }
+}
+export default {
+  extends: Bar,
+  mixins: [reactiveProp],
+  props: ["width", "height"],
+  data() {
+    return {
+      options: chartOptions
+    }
+  },
+  mounted() {
+    this.renderChart(this.chartData, this.options);
+  }
+};
+</script>
