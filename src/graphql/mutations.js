403adefca5ec1c9b40236a566fe5e45364b91644
David Maccarone <noreply@git.email>
2019-07-19 17:50:53 +1000

    run 'amplify push'

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -1,6 +1,15 @@
 // eslint-disable
 // this is an auto generated file. This will be overwritten
 
+export const castVote = `mutation CastVote($input: CastVoteInput!) {
+  castVote(input: $input) {
+    id
+    name
+    description
+    votes
+  }
+}
+`;
 export const createCandidate = `mutation CreateCandidate($input: CreateCandidateInput!) {
   createCandidate(input: $input) {
     id
--- src/graphql/subscriptions.js
+++ src/graphql/subscriptions.js
@@ -1,6 +1,15 @@
 // eslint-disable
 // this is an auto generated file. This will be overwritten
 
+export const onCastVote = `subscription OnCastVote {
+  onCastVote {
+    id
+    name
+    description
+    votes
+  }
+}
+`;
 export const onCreateCandidate = `subscription OnCreateCandidate {
   onCreateCandidate {
     id
